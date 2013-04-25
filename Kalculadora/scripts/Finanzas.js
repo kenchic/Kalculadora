bea4866856331fd9bc60a859172e1295bb82e89b
kenchic@gmail.com <kenchic@gmail.com>
2013-04-25 18:22:24 -0500

    Tabla de valores

--- Kalculadora/scripts/Finanzas.js
+++ Kalculadora/scripts/Finanzas.js
@@ -9,9 +9,22 @@ function CalcularCuotas()
     var txtNoCuotas = document.getElementById('txtNoCuotas');
     
     var numeroElevado = + Math.pow(1 + (txtIntereses.value/100), txtNoCuotas.value);   
-    var resultado= Monto.value * ( (txtIntereses.value/100) * numeroElevado ) / ( numeroElevado-1);
+    var pago= Monto.value * ( (txtIntereses.value/100) * numeroElevado ) / ( numeroElevado-1);
     
-    formResultado.innerHTML = 'Valor a pagar mensualmente: ' + parseInt(resultado);
+    var tablaResultado = '<table class="formatHTML5">';
+    tablaResultado += '<thead><tr><th>Cuota</th><th>Capital</th><th>Intereses</th></th><th>Pago</th><th>Saldo</th></tr></thead><tbody>';    
+    var capital = Monto.value;
+    var interes = 0;
+    
+    for (var i=1;i<=txtNoCuotas.value;i++)
+    {   
+        interes = parseFloat(capital * (txtIntereses.value/100)).toFixed(2);               
+        tablaResultado += '<tr><td>'+i+'</td><td>'+parseInt(pago-interes)+'</td><td>'+interes+'</td><td>'+Math.round(pago)+'</td><td>'+parseInt(capital-(pago-interes))+'</td></tr>';                
+        capital -= Math.round(pago-interes); 
+    }
+    tablaResultado += '</tbody></table>';
+    
+    formResultado.innerHTML = tablaResultado;
     formResultado.style.display = 'block';
     formIngreso.style.display = 'none';    
 }
@@ -29,3 +42,17 @@ function Limpiar() {
     formResultado.style.display = 'none';
     formIngreso.style.display = 'block';
 }
+
+//c: numero decimales; d: caracter decimal; t:caracter miles
+function FormatoMiles(c, d, t)
+{
+  var n = this, 
+  c = isNaN(c = Math.abs(c)) ? 2 : c, 
+  d = d == undefined ? "." : d, 
+  t = t == undefined ? "," : t, 
+  s = n < 0 ? "-" : "", 
+  i = parseInt(n = Math.abs(+n || 0).toFixed(c)) + "", 
+  j = (j = i.length) > 3 ? j % 3 : 0;
+  return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
+ }
+
